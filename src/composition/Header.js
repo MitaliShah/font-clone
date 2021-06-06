cf8841ec9c3a2b01033b2194cbc87357ee94e616
mitali shah <mitalishaleenshah@gmail.com>
2021-06-06 16:44:34 -0500

    styling for reset

--- src/composition/Header.js
+++ src/composition/Header.js
@@ -4,7 +4,7 @@ const Header =  () => {
   return (
     <React.Fragment>
       <header>
-          <h1><span className="logo">Google Font</span> Clone</h1>
+          <h1 className="logo">Google Font Clone</h1>
           <nav className="header-nav">
             <ul className="header-nav-ul">
               <li><a href="#main">CATALOG</a></li>
